35abf6a24d0d500b463a63d86d2cfa2e2b634982
Your Name201~ <abolore200@gmail.com>
2023-03-22 17:13:31 +0100

    updated

--- public/asset/js/chat.js
+++ public/asset/js/chat.js
@@ -1,13 +1,18 @@
 const form = document.querySelector('.ms-cd-fixed-bottom-navbar-col form')
-//on click
+//on click, send text
 form.addEventListener('submit', function(e){
     e.preventDefault()
     const textArea = document.querySelector('.ms-cd-fixed-bottom-navbar-col textarea').value
+    if(textArea.length === ''){
+        form[SubmitEvent] = false
+    }
     //every new chat text chat
-    const chatLi = document.createElement('li')
-    chatLi.textContent = textArea
+    const chatLi = document.createElement('div')
+    chatLi.innerHTML = `<li>${textArea}</li>`
     //append chat to the message box
-    const chatArea = document.querySelector('.chat-messages-hf-nd ul')
+    const chatArea = document.querySelector('.chat-messages-hf-nd')
     chatArea.appendChild(chatLi)
+
+    //clear textatrea
     form.reset()
 })
