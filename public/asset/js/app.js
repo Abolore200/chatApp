0283bf35f66180b28f581545fbb5cfcad58d14aa
Your Name201~ <abolore200@gmail.com>
2023-03-22 10:57:15 +0100

    updated

---
+++ public/asset/js/app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const email = document.querySelector('form input[type="email"]').value
+    const password = document.querySelector('form input[type="password"]').value
+    if(email ==='' || password === ''){
+        formClass.throwError()
+    } else {
+        window.location.href = 'welcome.html'
+    }
+})
+
+class formUI{
+    throwError(){
+        const error = document.createElement('div')
+        error.classList.add('error')
+        error.innerHTML = `
+            <p> fill all inputs </p>
+        `;
+        const errorBefore = document.querySelector('.input-jd-er-gs')
+        errorBefore.style.marginTop = '0px'
+        form.insertBefore(error, errorBefore)
+        setTimeout(function(){
+            error.remove()
+            errorBefore.style.removeProperty('margin-top', '30px')
+        },2000)
+    }
+}
+const formClass = new formUI()
