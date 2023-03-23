5dc1a5736a587d6a1dee04d73d5e46937ab9b969
Your Name201~ <abolore200@gmail.com>
2023-03-23 21:08:13 +0100

    updated

---
+++ public/asset/js/chatHome.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const profileBg = document.querySelector('.ms-cd-fixed-navbar-col-row-2 p')
+    profileBg.classList.add('addBg')
+    const profileColor = document.querySelector('.ms-cd-fixed-navbar-col-row-2 p a')
+    profileColor.classList.add('addColor')
+})
