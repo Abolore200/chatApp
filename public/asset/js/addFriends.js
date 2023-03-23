42616833ca704635d331c8c9e57dc202cc3df54f
Your Name201~ <abolore200@gmail.com>
2023-03-23 23:45:42 +0100

    updated

--- public/asset/js/addFriends.js
+++ public/asset/js/addFriends.js
@@ -4,3 +4,28 @@ document.addEventListener('DOMContentLoaded', function(){
     const profileColor = document.querySelector('.ms-cd-fixed-navbar-col-row-1 p a')
     profileColor.classList.add('addColor')
 })
+
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://randomuser.me/api/?results=2&nat=us', true)
+xhr.onload = function(){
+    if(this.status === 200){
+        const all = JSON.parse(this.responseText).results
+        let names = ""
+        for(let i = 0; i < all.length; i++){
+            // names += `${all[i].name.first} ${all[i].name.last}`
+            let nam;
+
+            // console.log(nam.first);
+            const nameOne = document.querySelector('.friend-one .friend-detail p')
+            const nameTwo = document.querySelector('.friend-two .friend-detail p')
+            
+            const arrays = [nameOne, nameTwo]
+            arrays.forEach(function(name,index){
+                nam = all[i].name.first + ' ' + all[i].name.last
+                name.innerHTML = nam[index]
+            })
+        }
+        // console.log(names);
+    }
+}
+xhr.send()
