cef72d18cf9c48377cf5a50ce93e05f5bde14a29
Your Name201~ <abolore200@gmail.com>
2023-03-24 11:03:32 +0100

    updated

--- public/asset/js/addFriends.js
+++ public/asset/js/addFriends.js
@@ -5,18 +5,65 @@ document.addEventListener('DOMContentLoaded', function(){
     profileColor.classList.add('addColor')
 })
 //api yo generate random names and profile pictures
-const xhr = new XMLHttpRequest()
-xhr.open('GET', 'https://randomuser.me/api/?results=2&nat=us', true)
-xhr.onload = function(){
+const newFriendAPI = new XMLHttpRequest()
+newFriendAPI.open('GET', 'https://randomuser.me/api/?results=4&nat=us', true)
+newFriendAPI.onload = function(){
     if(this.status === 200){
         const all = JSON.parse(this.responseText).results
         //generate random name for the friend lists
-        document.querySelector('.friend-one .friend-detail p').innerHTML = `${all[0].name.first} ${all[0].name.last}`
-        document.querySelector('.friend-two .friend-detail p').innerHTML = `${all[1].name.first} ${all[1].name.last}`
+        let newFriend = ""
+        for(let i =0; i < all.length; i++){
+            newFriend += `
+            <div class="new-friends">
+                <div class="friend-img">
+                    <img src="${all[i].picture.medium}" />
+                </div>
+                <div class="friend-detail">
+                    <p>${all[i].name.first} ${all[i].name.last}</p>
+                    <div class="acc-rem">
+                        <button>Add Friend</button>
+                        <button class="removeBtn">Remove</button>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+        document.querySelector('.sug-friend').innerHTML = newFriend
+   }
+}
+newFriendAPI.send()
 
-        //generate random profile pictures for the friend list
-        document.querySelector('.friend-one .friend-img').innerHTML = `<img src="${all[0].picture.medium}" />`
-        document.querySelector('.friend-two .friend-img').innerHTML = `<img src="${all[1].picture.medium}" />`
+const friendRequestAPI = new XMLHttpRequest()
+friendRequestAPI.open('GET', 'https://randomuser.me/api/?results=6&nat=us', true)
+friendRequestAPI.onload = function(){
+    if(this.status === 200){
+        const all = JSON.parse(this.responseText).results
+        let friendRequest = ""
+        for(let i =0; i < all.length; i++){
+            friendRequest += `
+                <div class="req-friends">
+                    <div class="friend-img">
+                        <img src="${all[i].picture.medium}" />
+                    </div>
+                    <div class="friend-detail">
+                        <p>${all[i].name.first} ${all[i].name.last}</p>
+                        <div class="acc-rem">
+                            <button>Accept</button>
+                            <button class="declineBtn">Decline</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+        document.querySelector('.friend-requests').innerHTML = friendRequest
     }
 }
-xhr.send()
+friendRequestAPI.send()
+
+// const btn = document.querySelector('.friend-one')
+// btn.addEventListener('click', function(e){
+//     if(e.target.classList.contains('removeBtn')){
+//         const rem = e.target.parentElement.parentElement.parentElement
+//         rem.remove()
+//     }
+// })
